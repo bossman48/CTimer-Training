d53fcfea4fa55c97ddd3aea6ddb4a8588dd3897a
bossman48 <kuzucu48@gmail.com>
2022-12-17 12:20:19 +0300

    ZTimer framework name was changed to CTimer

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from ZTimer import Ztimer
+from CTimer import Ctimer
 from Master import *
 import time 
 
-zTimer = Ztimer()
+cTimer = Ctimer()
 
 masterObject = Master()
 
 masterObject2 = Master()
 
-zTimer._createScheduledEvent(subscriber=masterObject,eventName="eventTwoSecond",msTimePeriod=2000)
+cTimer._createScheduledEvent(subscriber=masterObject,eventName="eventTwoSecond",msTimePeriod=2000)
 
-zTimer._subscribeScheduledEvent(subscriber=masterObject2,eventName="eventTwoSecond")
+cTimer._subscribeScheduledEvent(subscriber=masterObject2,eventName="eventTwoSecond")
 
-zTimer._createScheduledEvent(subscriber=masterObject,eventName="eventFiveSecond",msTimePeriod=5000)
+cTimer._createScheduledEvent(subscriber=masterObject,eventName="eventFiveSecond",msTimePeriod=5000)
 
 time.sleep(2)
 
-zTimer._removeSubscriberFromScheduledEvent(subscriber=masterObject2,eventName="eventTwoSecond")
-zTimer._removeScheduledEvent(eventName="eventTwoSecond")
+cTimer._removeSubscriberFromScheduledEvent(subscriber=masterObject2,eventName="eventTwoSecond")
+cTimer._removeScheduledEvent(eventName="eventTwoSecond")
